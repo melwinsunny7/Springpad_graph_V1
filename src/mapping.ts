da88fcb8b3298ecf5b7637598a20c4e93947b773
Melwin Sunny <melwin.sunny@phaethon.tech>
2022-05-23 10:28:27 +0530

    send-recieve subgrpah

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,3 +1,64 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import { Contract } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { FeePaidToTreasury, FundingDetails, FundTransferredToRWAO, RepaymentStatus } from "../generated/Sentrecieve/Sentrecieve"
+import { Loan, LoanRepayment } from "../generated/schema"
+
+export function handleFundingDetails( event: FundingDetails): void {
+  let id = event.params.loanId
+  let entity = Loan.load(id)
+
+  if (!entity) {
+    entity = new Loan(id)
+    entity.id = event.params.loanId
+  }
+  entity.LoanId = event.params.loanId
+  entity.AssetId = event.params.assetId
+  entity.NFTId = event.params.LNFTId
+  entity.rwaoId = event.params.rwaoId
+  entity.RepaymentDate = event.params.repaymentDate
+  entity.Tenure = event.params.tenure
+
+  entity.save()
+}
+
+export function handleFundTransferredToRWAO(event: FundTransferredToRWAO): void {
+  let id = event.params.loanId
+  let entity = Loan.load(id)
+
+  if (!entity) {
+    entity = new Loan(id)
+    entity.id = event.params.loanId
+  }
+  entity.LoanId = event.params.loanId
+  entity.FundingStatus = event.params.fundTrasferred
+
+  entity.save()
+  
+}
+
+export function handleRepaymentStatus(event: RepaymentStatus): void {
+  let id = event.params.loanId
+  let entity = LoanRepayment.load(id)
+
+  if (!entity) {
+    entity = new LoanRepayment(id)
+    entity.id = event.params.loanId
+  }
+  entity.LoanId = event.params.loanId
+  entity.RepaymentStatus = event.params.repaymentStatus
+
+  entity.save()
+
+}
+
+export function handleFeePaidToTreasury(event: FeePaidToTreasury): void {
+  let id = event.params.loanId
+  let entity = LoanRepayment.load(id)
+
+  if (!entity) {
+    entity = new LoanRepayment(id)
+    entity.id = event.params.loanId
+  }
+  entity.FeePaymentStatus = event.params.feePaymentStatus
+
+  entity.save()
+}
